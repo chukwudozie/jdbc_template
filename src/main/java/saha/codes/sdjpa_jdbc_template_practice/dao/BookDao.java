6db0d6723103d644df02688c05252292f10b5889
chukwudozie <emkychuks062@gmail.com>
2022-04-30 17:35:41 +0100

    add the findAllBooks method

--- src/main/java/saha/codes/sdjpa_jdbc_template_practice/dao/BookDao.java
+++ src/main/java/saha/codes/sdjpa_jdbc_template_practice/dao/BookDao.java
@@ -2,8 +2,11 @@ package saha.codes.sdjpa_jdbc_template_practice.dao;
 
 import saha.codes.sdjpa_jdbc_template_practice.domain.Book;
 
+import java.util.List;
+
 public interface BookDao {
 
+    List<Book> findAllBooks();
     Book getById(Long id);
 
     Book findByTitle(String title);
