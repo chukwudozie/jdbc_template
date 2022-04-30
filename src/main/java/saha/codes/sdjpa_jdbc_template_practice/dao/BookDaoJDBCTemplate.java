f62e5980a3ab3f6c98b8a3e0c07455670eda7d33
chukwudozie <emkychuks062@gmail.com>
2022-04-30 17:37:22 +0100

    add new BookDao Impl

---
+++ src/main/java/saha/codes/sdjpa_jdbc_template_practice/dao/BookDaoJDBCTemplate.java
@@ -0,0 +1,63 @@
+package saha.codes.sdjpa_jdbc_template_practice.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import saha.codes.sdjpa_jdbc_template_practice.domain.Book;
+
+import java.util.List;
+
+@Component
+public class BookDaoJDBCTemplate implements BookDao{
+
+    private final JdbcTemplate template;
+
+    public BookDaoJDBCTemplate(JdbcTemplate template) {
+        this.template = template;
+    }
+
+    @Override
+    public List<Book> findAllBooks() {
+        return null;
+    }
+
+    @Override
+    public Book getById(Long id) {
+        return template.queryForObject("SELECT * FROM book WHERE id = ?", getBookMapper(),id);
+    }
+
+    @Override
+    public Book findByTitle(String title) {
+        return template.queryForObject("SELECT * FROM book WHERE title = ?",getBookMapper(),title);
+    }
+
+    @Override
+    public Book saveBook(Book newBook) {
+         template.update("INSERT INTO book (isbn, publisher, title, author_id) VALUES " +
+                "(?, ?, ?, ?)",newBook.getIsbn(),newBook.getPublisher(),newBook.getTitle(),newBook.getAuthorId());
+         Long savedId = template.queryForObject("SELECT LAST_INSERT_ID()",Long.class);
+         return this.getById(savedId);
+    }
+
+    @Override
+    public Book updateBook(Book savedBook) {
+         template.update("UPDATE book SET isbn = ?, publisher = ?, title = ?, author_id = ? " +
+                "WHERE id = ?",savedBook.getIsbn(),savedBook.getPublisher(), savedBook.getTitle(),
+                savedBook.getAuthorId(), savedBook.getId());
+         Long bookId = template.queryForObject("SELECT LAST_INSERT_ID()",Long.class);
+         return this.getById(bookId);
+    }
+
+    @Override
+    public Integer getBookCount() {
+        return template.queryForObject("SELECT COUNT(*) FROM book",Integer.class);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        template.update("DELETE FROM book WHERE id = ?",id);
+    }
+
+    private BookMapper getBookMapper (){
+        return new BookMapper();
+    }
+}
---
+++ src/test/java/saha/codes/sdjpa_jdbc_template_practice/dao/BookDaoJdbcTemplateTest.java
@@ -0,0 +1,79 @@
+package saha.codes.sdjpa_jdbc_template_practice.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.dao.EmptyResultDataAccessException;
+import saha.codes.sdjpa_jdbc_template_practice.domain.Author;
+import saha.codes.sdjpa_jdbc_template_practice.domain.Book;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DataJpaTest
+@ComponentScan(basePackages = {"saha.codes.sdjpa_jdbc_template_practice.dao"})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class BookDaoJdbcTemplateTest {
+
+    @Autowired
+    @Qualifier(value = "bookDaoImpl")
+    private BookDao bookDao;
+    @Autowired
+    private AuthorDao authorDao;
+
+    @Test
+    void testGetById(){
+        Book book = bookDao.getById(3L);
+        assertThat(book).isNotNull();
+    }
+    @Test
+    public void testFindBookByTitle(){
+        Book book = bookDao.findByTitle("Clean Code");
+        assertThat(book).isNotNull();
+    }
+
+    @Test
+    public void testSaveBook(){
+        Book newBook = new Book("test book","2468","test author");
+        Author author = new Author("first name", "last name");
+        Author savedAuthor = authorDao.save(author);
+        newBook.setAuthorId(savedAuthor.getId());
+        Book saved = bookDao.saveBook(newBook);
+        assertThat(saved).isNotNull();
+    }
+
+    @Test
+    public void testUpdateBook(){
+        Book book = new Book("update title","test isbn", "test author");
+        Author author = new Author("first name", "last name");
+        Author savedAuthor = authorDao.save(author);
+        book.setAuthorId(savedAuthor.getId());
+        Book savedBook = bookDao.saveBook(book);
+        savedBook.setIsbn("updated test isbn");
+        Book updatedBook = bookDao.updateBook(savedBook);
+        assertThat(updatedBook.getIsbn()).isEqualTo("updated test isbn");
+
+    }
+
+
+    @Test
+    public void testBookCount(){
+        int count = bookDao.getBookCount();
+        System.out.println(count);
+        assertThat(count).isGreaterThan(0);
+    }
+
+    @Test
+    public void testDeleteBookById(){
+        Book testBook = new Book("test title", "123", "test publisher");
+        Book savedBook =bookDao.saveBook(testBook);
+        bookDao.deleteById(savedBook.getId());
+        assertThrows(EmptyResultDataAccessException.class,() -> {
+            bookDao.getById(savedBook.getId());
+        });
+    }
+
+}
